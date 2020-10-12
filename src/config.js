ba63e45071c825a3b2392762595a4febe8a560e1
Danny Shi <dannyshi.sr@gmail.com>
2020-10-12 17:33:11 -0400

    ready to push to herkou

--- src/config.js
+++ src/config.js
@@ -3,5 +3,6 @@ require("dotenv").config();
 module.exports = {
   PORT: process.env.PORT || 8000,
   NODE_ENV: process.env.NODE_ENV || "development",
-  DB_URL: process.env.DB_URL || "postgresql://dannyshi@localhost/noteful",
+  DATABASE_URL:
+    process.env.DATABASE_URL || "postgresql://dannyshi@localhost/noteful",
 };
