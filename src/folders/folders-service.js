55a0e09ceb2603e123fba6bfcb3363ba49a39571
Danny Shi <dannyshi.sr@gmail.com>
2020-11-16 17:37:03 -0500

    updated router and service files

--- src/folders/folders-service.js
+++ src/folders/folders-service.js
@@ -1,24 +1,18 @@
 const FoldersService = {
   getAll(knex) {
-    return knex.select("*").from("noteful_folders");
+    return knex.select('*').from('noteful_folders')
   },
   insertFolder(knex, newArticle) {
-    return knex
-      .insert(newArticle)
-      .into("noteful_folders")
-      .returning("*")
-      .then((rows) => {
-        return rows[0];
-      });
+    return knex.insert(newArticle).into('noteful_folders').returning('*').then(rows => { return rows[0] })
   },
   getById(knex, id) {
-    return knex.select("*").from("noteful_folders").where("id", id).first();
+    return knex.select('*').from('noteful_folders').where('id', id).first()
   },
   deleteById(knex, id) {
-    return knex("noteful_folders").where({ id }).delete();
+    return knex('noteful_folders').where({ id }).delete()
   },
   updateArticle(knex, id, updateArticle) {
-    return knex("noteful_folders").where({ id }).update(updateArticle);
-  },
-};
-module.exports = FoldersService;
+    return knex('noteful_folders').where({ id }).update(updateArticle)
+  }
+}
+module.exports = FoldersService
